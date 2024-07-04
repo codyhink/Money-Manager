11422f1aa53182e3a9054404a4ab740782749716
unknown <chink@chink-FD429K3.northamerica.cerner.net>
2024-07-04 13:27:31 -0500

    Updated code to make more DRY, added functionality of updating total in accounts

--- app.js
+++ app.js
@@ -5,52 +5,79 @@ const submitButton = document.getElementById("submit");
 const form = document.getElementById("addNewTransaction");
 const trash = document.getElementById("trash");
 const listItem = document.querySelectorAll(".item");
+const checkingAccount = document.querySelector("#checking");
+const savingsAccount = document.querySelector("#savings");
+
+let checkingTotal = 5600;
+let savingsTotal = 15000;
+
+checkingAccount.textContent = `$${addCommasToNumber(checkingTotal)}`;
+savingsAccount.textContent = `$${addCommasToNumber(savingsTotal)}`;
 
 let redGreen;
 
 // Delete Transaction
 transactions.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
-  }
+	if (e.target.classList.contains("delete")) {
+		e.target.parentElement.remove();
+	}
 });
 
 // Capture user inputs for new item
-submitButton.addEventListener("click", function addTransaction(e) {
-  e.preventDefault();
-  const addedDescription = description.value.trim();
-  const userAmount = amount.value.trim();
-  function radioValue() {
-    const creditDebit = document.getElementsByName("plus-minus");
-    for (i = 0; i < creditDebit.length; i++) {
-      if (creditDebit[i].checked) {
-        transactionCat = creditDebit[i].value;
-      }
-    }
-  }
+submitButton.addEventListener("click", addTransaction);
 
-  radioValue();
-  if (transactionCat == "expense") {
-    redGreen = "credit";
-  } else {
-    redGreen = "debit";
-  }
+// Add a new transaction
+function addTransaction(e) {
+	e.preventDefault();
+	const addedDescription = description.value.trim();
+	const userAmount = amount.value.trim();
+	const transactionCat = document.querySelector(
+		'input[name="plus-minus"]:checked'
+	).value;
+	const accountSelected = document.querySelector("#choose-account").value;
+	console.log(accountSelected);
 
-  newTransaction(addedDescription, redGreen, userAmount);
-  form.reset();
-});
+	transactionCat === "expense" ? (redGreen = "credit") : (redGreen = "debit");
+
+	newTransaction(addedDescription, redGreen, userAmount);
+	updateAccount(accountSelected, transactionCat, userAmount);
+
+	accountSelected === "checking"
+		? addCommasToNumber(checkingTotal)
+		: addCommasToNumber(savingsTotal);
+
+	form.reset();
+}
 
 // Template for new item in transactions
 const newTransaction = (addedDescription, redGreen, userAmount) => {
-  const newItem = `
-  <div class="item">
+	const newItem = document.createElement("div");
+	newItem.classList.add("item");
+	newItem.innerHTML = ` 
   <p>${addedDescription}</p>
   <p class="${redGreen}">$${userAmount}</p>
   <i class="fa-solid fa-trash delete"></i>
-
-
-  </div>
   `;
 
-  transactions.innerHTML += newItem;
+	transactions.appendChild(newItem);
 };
+
+function updateAccount(account, plusMinus, amount) {
+	if (account === "checking") {
+		plusMinus === "expense"
+			? (checkingTotal -= amount)
+			: (checkingTotal += amount);
+	} else {
+		plusMinus === "expense"
+			? (savingsTotal -= amount)
+			: (savingsTotal += amount);
+	}
+
+	checkingAccount.textContent = `$${addCommasToNumber(checkingTotal)}`;
+	savingsAccount.textContent = `$${addCommasToNumber(savingsTotal)}`;
+}
+
+// Add commas to number in the DOM
+function addCommasToNumber(number) {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
